a3e6cbfa4e4ddb5e3cf89f682e817bbd99aef5d3
Abosede Awoniyi <awoniy.abosede@gmail.com>
2022-06-28 15:46:26 +0100

    make it better

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const morgan = require('morgan')
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.listen(PORT, ()=>{
+    console.log('Listening')
+})
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(morgan('dev'))
+
+app.get('/', (req, res) =>{
+    res.render('index')
+})
