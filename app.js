e1949d6355d6f9cdc40e4dc4abe9aa4dc7a7bd4b
Abosede Awoniyi <awoniy.abosede@gmail.com>
2022-06-28 16:41:35 +0100

    fixed untracked files

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.get('/', (req, res) =>{
     res.render('index', {title : 'On Time Delivery App'})
 })
 
-app.get('/', (req, res) =>{
+app.get('/docs', (req, res) =>{
     res.render('docs', {title : 'On Time Delivery App'})
 })
